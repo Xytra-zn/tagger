1c019afa5aa07ddfdc8b136118f43fe65fcd5039
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-11-25 18:15:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession  # Add this line
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
+
+# Replace these with your actual environment variable names
+API_ID = int(os.getenv('TELEGRAM_API_ID'))
+API_HASH = os.getenv('TELEGRAM_API_HASH')
+BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TELETHON_SESSION = os.getenv('TELETHON_SESSION')
+
+# Check if required environment variables are provided
+if not (API_ID and API_HASH and BOT_TOKEN and TELETHON_SESSION):
+    raise ValueError("API_ID, API_HASH, TOKEN, and TELETHON_SESSION are required.")
+
+# Initialize the Telethon client
+client = TelegramClient(StringSession(TELETHON_SESSION), API_ID, API_HASH)
+
+# Example event handling for tagging users
+@client.on(events.NewMessage(pattern="^/tagall"))
+async def tag_all(event):
+    chat_id = event.chat_id
+    async for user in client.iter_participants(chat_id):
+        await event.respond(f"Tagging {user.first_name}!")
+
+# Add more event handlers and bot functionality as needed
+
+# Run the bot
+client.start()
+client.run_until_disconnected()
