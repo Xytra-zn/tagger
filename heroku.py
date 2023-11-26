1377ab48449f8e032bb58eb27424ab9210cee335
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-11-26 10:01:54 +0530

    Update heroku.py

--- heroku.py
+++ heroku.py
@@ -8,7 +8,7 @@ load_dotenv()
 # For example, setting environment variables
 os.environ.setdefault("TELEGRAM_API_ID", "your_api_id")
 os.environ.setdefault("TELEGRAM_API_HASH", "your_api_hash")
-os.environ.setdefault("TELEGRAM_BOT_TOKEN", "your_bot_token")
+os.environ.setdefault("TELEGRAM_TOKEN", "your_bot_token")
 os.environ.setdefault("TELETHON_SESSION", "your_telethon_session")
 
 # You can add more Heroku-specific configurations if needed
